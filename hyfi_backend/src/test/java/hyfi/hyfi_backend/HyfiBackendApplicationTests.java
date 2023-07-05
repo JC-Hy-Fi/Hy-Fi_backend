43c3cf9018d484778ad34bf80d47fa40d787e558
RyuChaeHyun <lch010201@gmail.com>
2023-07-05 21:58:07 +0900

    feat: project 생성

---
+++ hyfi_backend/src/test/java/hyfi/hyfi_backend/HyfiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hyfi.hyfi_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HyfiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
